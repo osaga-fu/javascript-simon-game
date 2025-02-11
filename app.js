9df975764903f2d6f1f907fd335ae704ef11c655
osaga-fu <151243613+osaga-fu@users.noreply.github.com>
2025-02-11 20:24:13 +0100

    Add on app.jss lighting effect on panels

---
+++ app.js
@@ -0,0 +1,24 @@
+const redButton = document.querySelector('.red')
+const blueButton = document.querySelector('.blue');
+const greenButton = document.querySelector('.green');
+const yellowButton = document.querySelector('.yellow');
+
+const sequence = [redButton, blueButton, greenButton, yellowButton];
+
+const flash = (panel) => {
+  return new Promise((resolve, reject) => {
+    panel.className += " active";
+    setTimeout(() => {
+      panel.className = panel.className.replace("active", "");
+      resolve();
+    }, 2000);
+  });
+};
+
+const main = async () => {
+  for (let panel of sequence) {
+    await flash(panel);
+  }
+};
+
+main();
