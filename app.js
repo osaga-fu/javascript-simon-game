d79bd935d658926c55c23c263cc84893e7eaa833
osaga-fu <151243613+osaga-fu@users.noreply.github.com>
2025-02-17 13:19:25 +0100

    Add sound when panels flash

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const greenButton = document.querySelector(".green");
 const yellowButton = document.querySelector(".yellow");
 
 const panels = [redButton, blueButton, greenButton, yellowButton];
+const sounds = {
+  red: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3"),
+  blue: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3"),
+  green: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3"),
+  yellow: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3")
+}
 
 const getRandomPanel = () => {
   const randomIndex = Math.floor(Math.random() * 4);
@@ -12,6 +18,16 @@ const getRandomPanel = () => {
 
 const flash = (panel) => {
   return new Promise((resolve, reject) => {
+    if (panel.classList.contains('red')) {
+      sounds.red.play();
+    } else if (panel.classList.contains('blue')) {
+      sounds.blue.play();
+    } else if (panel.classList.contains('green')) {
+      sounds.green.play();
+    } else if (panel.classList.contains('yellow')) {
+      sounds.yellow.play();
+    }
+
     panel.className += " active";
     setTimeout(() => {
       panel.className = panel.className.replace("active", "");
